1555882cb19125ac92e556991201e38b91018a6e
MatheusBamberg <matheusbambeg2005@gmail.com>
2025-11-17 21:15:55 -0300

    Adição dos Scrips para Criação de Grupos

---
+++ Scripts/Script Politica de Acesso - Monamu.sql
@@ -0,0 +1,12 @@
+-- Criação dos Grupos de Funcionários:
+create group gerente;
+create group funcionario;
+
+-- Atribuição das Permissões sobre as Tabelas:
+grant select, insert, delete, update
+on cliente, condicional, desconto, fornecedor, funcionario, item_condicional, item_venda, loja, pessoa, venda
+to gerente;
+
+grant select, insert, update
+on cliente, condicional, desconto, item_condicional, item_venda, pessoa, produto, venda
+to funcionario;
