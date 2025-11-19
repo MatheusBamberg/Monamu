f0782aac8f0a9ce49a7409301c862bf77f0570b4
MatheusBamberg <matheusbambeg2005@gmail.com>
2025-11-18 23:19:46 -0300

    Inserção de um novo Trigger

--- Scripts/Script de Triggers - Monamu.sql
+++ Scripts/Script de Triggers - Monamu.sql
@@ -39,5 +39,54 @@ values ( 'Black Friday', 25, current_timestamp, '2025-12-20', 5);
 
 delete from desconto 
 where coddsc = 7;
+
 -----------------------------------------------------------------------
 
+-- Trigger para controle de Integridade do Sistema. Confirmação de Deleção de Clientes:
+create or replace function confirmacao_delete()
+returns trigger
+as
+$Body$
+begin
+	if TG_OP = 'DELETE' then
+		if current_setting('app.confirma_delete', true) is distinct from ('true') then
+			raise exception 'Delete bloqueado: necessário confirmar antes de executar.';
+		end if;
+	end if;
+	
+	return OLD;
+end
+$Body$
+language plpgsql;
+
+drop trigger if exists delete_cliente_bf_tg on cliente;
+
+-- Inserindo Trigger:
+create trigger delete_cliente_bf_tg
+before delete
+on cliente
+for each row
+execute procedure confirmacao_delete();
+
+-- Testes
+--Inserts para testar
+select * from cliente;
+select * from pessoa;
+
+insert into pessoa (nompes, emapes, sexpes, telpes, ruapes, baipes, cidpes, estpes)
+values ('carlos', 'carlos@gmail.com', 'M', '9', 'rur', 'la', 'aqui', 'sc');
+
+insert into cliente (codcli, cpfcli, datcadcli)
+values (6, '21512577', current_timestamp);
+
+
+delete from cliente
+where codcli = 6;
+
+begin;
+
+set local app.confirma_delete = true;
+delete from cliente
+where codcli = 6;
+
+rollback;
