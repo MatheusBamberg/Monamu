e40b31b987aa5d722156b5a667b75e1cd17790d8
moises zilles <moiseszilles@gmail.com>
2025-11-18 22:24:59 -0300

    Update Funcionario.java
    Complemento da classe Funcionario.java

--- monamu/src/main/java/br/edu/unoesc/monamu/model/Funcionario.java
+++ monamu/src/main/java/br/edu/unoesc/monamu/model/Funcionario.java
@@ -18,20 +18,76 @@ import jakarta.persistence.Table;
 @PrimaryKeyJoinColumn(name = "codpes")
 public class Funcionario extends Pessoa {
 
-	@Column(name = "cpffun", nullable = false, unique = true)
+	/**
+	 * Número de Cadastro de Pessoa Física (CPF) do funcionário.
+	 */
+	@Column(name = "cpffun", length = 14, nullable = false, unique = true) // 11 dígitos + formatação
 	private String cpf;
 
-	@Column(name = "carfun")
+	/**
+	 * Cargo ou função do funcionário na loja.
+	 */
+	@Column(name = "carfun", length = 50)
 	private String cargo;
 
+	/**
+	 * Data de admissão do funcionário na loja.
+	 */
 	@Column(name = "datadmfun")
 	private LocalDateTime dataAdmissao;
 
-	@Column(name = "senfun")
+	/**
+	 * Senha de acesso do funcionário ao sistema (idealmente armazenada de forma segura/hash).
+	 */
+	@Column(name = "senfun", length = 60) // Comprimento para hash (ex: bcrypt)
 	private String senha;
 
+	/**
+	 * Loja a qual o funcionário está vinculado.
+	 */
 	@ManyToOne
 	@JoinColumn(name = "codloj", nullable = false)
 	private Loja loja;
 
+	// --- Getters e Setters ---
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public String getCargo() {
+		return cargo;
+	}
+
+	public void setCargo(String cargo) {
+		this.cargo = cargo;
+	}
+
+	public LocalDateTime getDataAdmissao() {
+		return dataAdmissao;
+	}
+
+	public void setDataAdmissao(LocalDateTime dataAdmissao) {
+		this.dataAdmissao = dataAdmissao;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	public Loja getLoja() {
+		return loja;
+	}
+
+	public void setLoja(Loja loja) {
+		this.loja = loja;
+	}
 }
