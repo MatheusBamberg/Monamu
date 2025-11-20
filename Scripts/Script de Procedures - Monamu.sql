4059e2e7bf275b44d7af1113814fe486845a5dc7
MatheusBamberg <matheusbambeg2005@gmail.com>
2025-11-20 18:53:10 -0300

    Criação das Procedures

---
+++ Scripts/Script de Procedures - Monamu.sql
@@ -0,0 +1,45 @@
+-- Criando procedure para calcular o total da venda com desconto:
+create or replace function venda_desconto(total numeric, desconto numeric)
+returns numeric
+as
+$Body$
+declare
+	resultado numeric(10, 2);
+
+begin
+	resultado := ((total) - (total * (desconto / 100))); 
+
+	return resultado;
+end
+$Body$
+language plpgsql;
+
+-- Teste da procedure:
+select
+    v.codven,
+    venda_desconto(v.totven, d.valdsc) "Total Com Desconto"
+from Venda v
+join Desconto d on d.coddsc = v.coddsc;
+
+
+-- Valor total de cada produto em estoque:
+create or replace function total_cada_produto(qtd_produto int4, vlr_produto numeric(6,2))
+returns numeric
+as
+$Body$
+declare
+	resultado numeric (10, 2);
+
+begin
+	resultado := (qtd_produto * vlr_produto);
+
+	return resultado;
+end
+$Body$
+language plpgsql;
+
+-- Teste da procedure:
+select
+	p.nompro,
+	total_cada_produto(p.qtdestpro, p.vendpro) "Valor Total"
+from produto p;
